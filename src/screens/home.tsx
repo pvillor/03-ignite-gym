b3410e3af5061df0701b0ca2c4f2c68c71be1d7c
paulo.guerra <paulo.guerra@flex-am.com.br>
2025-01-13 09:47:25 -0400

    feat: group component

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,10 +1,28 @@
+import { Group } from "@components/group";
 import { HomeHeader } from "@components/home-header";
-import { VStack } from "@gluestack-ui/themed";
+import { HStack, VStack } from "@gluestack-ui/themed";
+import { useState } from "react";
 
 export function Home() {
+  const [groupSelected, setGroupSelected] = useState('dorsal')
+
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <HStack>
+        <Group
+          name="Dorsal" 
+          isActive={groupSelected === 'dorsal'} 
+          onPress={() => setGroupSelected('dorsal')} 
+        />
+
+        <Group
+          name="Ombro" 
+          isActive={groupSelected === 'ombro'} 
+          onPress={() => setGroupSelected('ombro')} 
+        />
+      </HStack>
     </VStack>
   )
 }
