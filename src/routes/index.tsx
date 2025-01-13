a0dfe3e84b2d594f1c5eda21e561e970adb813dd
paulo.guerra <paulo.guerra@flex-am.com.br>
2025-01-13 09:10:20 -0400

    feat: navigation by bottom tabs

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { DefaultTheme, NavigationContainer } from "@react-navigation/native"
 import { AuthRoutes } from "./auth"
 import { gluestackUIConfig } from "../../config/gluestack-ui.config"
 import { Box } from "@gluestack-ui/themed"
+import { AppRoutes } from "./app"
 
 export function Routes() {
   const theme = DefaultTheme
@@ -11,7 +12,7 @@ export function Routes() {
   return (
     <Box flex={1} bg="$gray700">
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        <AppRoutes />
       </NavigationContainer>
     </Box>
   )
